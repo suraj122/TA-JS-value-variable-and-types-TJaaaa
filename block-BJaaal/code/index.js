b4accb42186a05dba2cdf1407b8498a0e6038b20
suraj_122 <122suraj0@gmail.com>
2021-04-01 10:47:13 +0530

    solve exercise for template literal

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -5,6 +5,9 @@
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
 */
 
+const userName = "Suraj";
+let bio = "Learning to code";
+
 let firstName = "Arya";
 let lastName = "Stark";
 
@@ -12,6 +15,9 @@ let lastName = "Stark";
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+
+alert(`I am ${userName} and my bio is ${bio}`);
+alert(`I'm ${firstName} ${lastName} of winterfell`);
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
@@ -28,9 +34,22 @@ const taxPercent = 10;
   - Above message should be in two lines
 */
 
+alert(`The total amount is ${amount}
+the tax amount is ${(amount * taxPercent) / 100}
+`);
+
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
     the tax amount is [(amount * taxPercent) / 100]
   `
   - Above message should be in two lines
 */
+
+alert(
+  "The total amount is" +
+    " " +
+    amount +
+    "\n the tax amount is" +
+    " " +
+    (amount * taxPercent) / 100
+);
